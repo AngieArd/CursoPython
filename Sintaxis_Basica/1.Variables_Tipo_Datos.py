03267a9f2b53d7eb6396c50b313282af4f9b85df
AngieArd <acardila657@misena.edu.co>
2023-06-05 11:06:10 -0500

    Update 1.Variables_Tipo_Datos.py
    Information about variables and the four simple data types handled in Python is added.

---
+++ Sintaxis_Basica/1.Variables_Tipo_Datos.py
@@ -0,0 +1,78 @@
+
+                    #Variables: 
+# son etiquetas que se utilizan para hacer referencia a un valor en la memoria
+#Para crear una variable en Python, simplemente se le asigna un valor utilizando 
+# el operador de asignación "=" seguido del valor que se desea asignar. 
+# Por ejemplo
+
+x = 5 
+X = 6  #En Python las variables son sensibles a mayusculas y minusculas
+
+
+
+
+#TIPOS DE DATOS
+#Python tiene cuatro tipos de datos SIMPLES: enteros, flotantes, booleanos y cadenas o Strings. 
+
+#Numero entero (int)
+2
+3
+4
+
+# se utilizan para representar datos numéricos, específicamente números enteros
+
+year = 2021
+dia = 7
+edad = 26
+temperatura = -5
+angulo = -45
+
+
+##Numero flotante (float)
+2.3
+2.4
+4.5
+
+#se utilizan para representar números de coma flotante o con decimales.
+
+pi = 3.1416
+estatura = 1.55
+peso = 72.6
+temperatura = -5.55
+edad = 32.7
+
+
+##Datos Booleanos (bool)
+True
+False
+
+#son tipos de datos binarios, es decir que pueden tomar los valores: Verdadero (True) y Falso (False).
+
+esta_frio = True
+es_bajo = False
+
+#Son útiles para expresiones con condicionales y de comparaciones.
+
+
+##Texto - String (str)
+
+'Cadena de texto'
+"Cadena de texto"
+"""Cadena 
+de
+texto""" #con triple comillas podemos bajar a otra linea de texto
+
+profesor = "Sergio Castaño Giraldo"
+web = 'Control Automático Educación'
+cursos = """
+1. Python
+2. Matlab
+3. MicroPython
+4. Arduino
+5. PIC
+6. Control de Procesos
+"""
+
+
+
+
